d9f34477813c2af2ca198b7bbce926709c1495b8
Riccardo Melchiorri <melchior@slac.stanford.edu>
2025-03-04 15:05:37 -0800

    changes for jungfrau detector

--- status/firmware_String.sh
+++ status/firmware_String.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #!/bin/bash
 
-ssh -X drp-neh-ctl002 clush -w drp-srcf-cmp0[01-50] cat /proc/datadev_1 | grep "Build String" > BuildString.txt
+ssh -X drp-neh-ctl002 clush -w drp-srcf-cmp0[01-60] cat /proc/datadev_0 | grep "Build String" > BuildString.txt
 
