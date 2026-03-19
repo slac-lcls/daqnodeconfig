c00ced558a0617a60960e99461057f85eda06b75
Riccardo Melchiorri <melchior@slac.stanford.edu>
2026-03-19 15:04:01 -0700

    debugging version 1.0.0

--- status/check_weka_status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ssh -X drp-neh-ctl002 clush -w drp-srcf-cmp0[01-50] ls /cds/drpsrcf/ > weka_drps.txt
-
--- status/firmware_String.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-
-ssh -X drp-neh-ctl002 clush -w drp-srcf-cmp0[01-60] cat /proc/datadev_0 | grep "Build String" > BuildString.txt
-
